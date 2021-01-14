518ff4c0d3ef1432cf59c8bc49df52af144f9058
sehmim <sehmim.al@gmail.com>
2021-01-14 16:35:30 -0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Sehmim Haque on 2021-01-14.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
